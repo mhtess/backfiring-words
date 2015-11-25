beec43c9f7712100b1d492c7694b0fefa46b7a01
elliechestnut <eleanor.chestnut@gmail.com>
2015-11-25 11:02:53 -0800

    Update stims.js

--- experiments/posterior/js/stims.js
+++ experiments/posterior/js/stims.js
@@ -241,4 +241,97 @@ var stimuli = [
 }
 	]
 	
-	//TEST
+var characters = [
+{
+	name: "Daniel",
+	gender: "male"
+},
+{
+	name: "David",
+	gender: "male"
+},
+{
+	name: "Alexander",
+	gender: "male"
+},
+{
+	name: "Gabriel",
+	gender: "male"
+},
+{
+	name: "Anthony",
+	gender: "male"
+},
+{
+	name: "William",
+	gender: "male"
+},
+{
+	name: "John",
+	gender: "male"
+},
+{
+	name: "Tom"
+	gender: "male"
+},
+{
+	name: "Lucas"
+	gender: "male"
+},
+{
+	name: "Michael"
+	gender: "male"
+},
+{
+	name: "Eric"
+	gender: "male"
+},
+{
+	name: "Cameron"
+	gender: "male"
+},
+{
+	name: "Angela"
+	gender: "female"
+},
+{
+	name: "Jennifer"
+	gender: "female"	
+},
+{
+	name: "Monica"
+	gender: "female"
+},
+{
+	name: "Melinda"
+	gender: "female"
+},
+{
+	name: "Veronica"
+	gender: "female"	
+},
+{
+	name: "Alicia"
+	gender: "female"
+},
+{
+	name: "Maya"
+	gender: "female"
+},
+{
+	name: "Sophia"
+	gender: "female"
+},
+{
+	name: "Kim"
+	gender: "female"
+},
+{
+	name: "Laura"
+	gender: "female"	
+},
+{
+	name: "Julia"
+	gender: "female"
+}
+]	
