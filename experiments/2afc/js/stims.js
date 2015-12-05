9dc7bb59ee275baddaeadf4fc86b8e72fa2b4372
mhtess <mtessler@stanford.edu>
2015-12-05 14:44:57 -0800

    draft of forced choice expt

---
+++ experiments/2afc/js/stims.js
@@ -0,0 +1,28 @@
+var stimuli = [
+{
+	item: "vegetarian",
+	prompt: "Steve is having a hard time remembering who is roommate Jonathon is dating. He knows it is either Jessica or Stephanie.",
+	pragmatic: "Jonathon arrives home from a date. Steve asks, ''How was the date?'' Jonathon says, ''It was great. We ordered vegetarian food.''",
+	literal: "Steve notices on the kitchen counter a receipt for Jonathon’s most recent date. On the receipt, Steve saw that they ordered vegetarian food.",
+	question: "Who is Jonathon dating?",
+	frequency: {
+		high: "Jessica, he knows, is vegetarian and never eats meat.",
+		medium: "Jessica, he knows, is sort-of a vegetarian and eats meat only once a week."	
+	},
+	foil: "Stephanie, he knows, eats whatever.",
+	likely: "Jessica",
+	unlikely: "Stephanie"
+},{
+	item: "music_lesson",
+	prompt: "Sally is having a hard time remembering who her son has for his elementary school teacher. She knows it is either Ms. Smith or Ms. Johnson.",
+	pragmatic: "At the dinner table her son Billy says: ''Mommy, the teacher taught music today.''",
+	literal: "At the dinner table, she notices her son Billy’s composition notebook, which had his daily schedule on it. It read that there was a music lesson today.",
+	question: "Who is Billy's teacher?",
+	frequency: {
+		high: "Ms. Smith, she knows, has a musical lesson incorporated into her class every day.",
+		medium: "Ms. Smith, she knows, has a musical lesson 4 out of 5 days."	
+	},
+	foil: "Ms. Johnson, she knows, only has a musical lesson occasionally.",
+	likely: "Ms. Smith",
+	unlikely: "Ms. Johnson"
+}]
