70b45bbc1bddb02a413807d5bb5cecf4bba277d7
mhtess <mtessler@stanford.edu>
2015-12-14 15:26:42 -0800

    more refinment of speakermanipulation condition

--- experiments/2afc/js/stims.js
+++ experiments/2afc/js/stims.js
@@ -1,18 +1,32 @@
 var stimuli = [
+// {
+// 	item: "girlfriend_vegetarian",
+// 	prompt: "Steve is having a hard time remembering which of his friends his roommate Jonathon is dating.<br>He knows it is either Jessica or Stephanie.",
+// 	pragmatic2: "Jonathon comes home from a date.<br>Steve asks, ''How was the date?''<br>Jonathon says, 'Oh good. She ordered vegetarian tonight.'",
+// 	literal: "Steve notices on the kitchen counter a receipt for Jonathon's date earlier that night.<br>On the receipt, Steve saw that his date ordered vegetarian tonight.",
+// 	question: "Who is Jonathon dating?",
+// 	frequency: {
+// 		high: "<strong>Jessica</strong>, he knows, is a strict vegetarian and never eats meat.",
+// 		medium: "<strong>Jessica, he knows, is sort-of a vegetarian and eats meat only once a week."	
+// 	},
+// 	foil: "<strong>Stephanie</strong>, he knows, eats whatever.",
+// 	likely: "Jessica",
+// 	unlikely: "Stephanie",
+// 	speakermanipulation2: "Jonathon comes home from a date.<br>Steve asks, 'How was the date? Did she order vegetarian tonight?'<br>Jonathon says, 'Oh good, and yes. She ordered vegetarian tonight.'",
+// },
 {
-	item: "girlfriend_vegetarian",
-	prompt: "Steve is having a hard time remembering which of his friends his roommate Jonathon is dating.<br>He knows it is either Jessica or Stephanie.",
-	pragmatic2: "Jonathon comes home from a date.<br>Steve asks, ''How was the date?''<br>Jonathon says, 'Oh good. She ordered vegetarian tonight.'",
-	literal: "Steve notices on the kitchen counter a receipt for Jonathon's date earlier that night.<br>On the receipt, Steve saw that his date ordered vegetarian tonight.",
-	question: "Who is Jonathon dating?",
+	item: "tenant_rentcheck",
+	prompt: "X is having a hard time remembering which of his friends Y has been subletting to for the past year.<br>He knows it is either A or B.",
 	frequency: {
-		high: "<strong>Jessica</strong>, he knows, is a strict vegetarian and never eats meat.",
-		medium: "<strong>Jessica, he knows, is sort-of a vegetarian and eats meat only once a week."	
+		high: "<strong>A</strong>, he knows, turns in his rent on time every month.",
 	},
-	foil: "<strong>Stephanie</strong>, he knows, eats whatever.",
-	likely: "Jessica",
-	unlikely: "Stephanie",
-	speakermanipulation2: "Jonathon comes home from a date.<br>Steve asks, 'How was the date? Did she order vegetarian tonight?'<br>Jonathon says, 'Oh good, and yes. She ordered vegetarian tonight.'",
+	foil: "<strong>B</strong>, he knows, only occasionally turns in his rent on time.",
+	pragmatic: "X and Y are playing basketball.<br>Y says, 'My tenant turned in his rent on time this month.'",
+	literal: "X and Y meet up at Y's apartment to play basketball. X notices that a rent check on Y's doorstep. His tenant paid his rent on time this month.<br>",
+	question: "Who is Jonathon dating?",
+	likely: "X",
+	unlikely: "Y",
+	speakermanipulation: "X and Y meet up at Y's apartment to play basketball. <br>X is helping Y fill out a survey for the city census, while X gets ready.  Y reads out loud, 'Did your tenant pay his rent on time this month?'  X replies, 'Yes, my tenant paid his rent on time this month.'",
 },
 {
 	item: "teacher_music",
@@ -32,19 +46,19 @@ var stimuli = [
 },
 {
  	item: "professor_attendance",
- 	prompt: "Ed is having a hard time remembering which professor is teaching his daughter Lauren's philosophy class. He knows it is either Professor Burke or Professor Fought.",
+ 	prompt: "Ed is having a hard time remembering which professor is teaching his daughter's philosophy class. He knows it is either Professor Burke or Professor Fought.",
  	pragmatic: "When his daughter comes home after class, she says to him, 'My professor took attendance today.'",
  	pragmatic2: "When his daughter comes home after class, Ed asks her: 'How was your philosophy class?'.<br> She says, 'Oh good. The professor took attendance today.'",
- 	literal:"Ed had to drop some thing off in the philosophy department today. He was by a classroom where he says his daughter in her philosophy class. The professor was taking attendance.",
- 	question: "Who is Lauren's professor?",
+ 	literal:"Ed had to return a book to the Philosophy department today. He walked by a classroom where he saw his daughter in her philosophy class. He heard the professor reading off names. The professor took attendance today.",
+ 	question: "Who is Ed's daughter's professor?",
  	frequency: {
  		high: "Professor Burke, he knows, always takes attendance.",
  		medium:	"Professor Burke, he knows, almost always takes attendance."
  	},
- 	foil: "Professor Fought, he knows, only occasionally takes attendance.",
+ 	foil: "Professor Carr, he knows, only occasionally takes attendance.",
  	likely: "Professor Burke",
- 	unlikely: "Professor Fought",
- 	speakermanipulation: "When his daughter comes home after class, she has to fill out a survey for school.  She asks Ed if he can fill it out for her while she puts her things away.  Ed reads out loud the first question on the list, which is, 'Did your professor take attendance today?'  Lauren replies, 'Yes, My professor took attendance today.'",
+ 	unlikely: "Professor Carr",
+ 	speakermanipulation: "His daughter comes home after class. Ed helps her fill out a university survey, while she puts her book away. Ed reads out loud, 'Did your professor take attendance today?'  His daughter replies, 'Yes. My professor took attendance today.'",
  	speakermanipulation2: "When his daughter comes home after class, Ed asks her: 'How was your philosophy class? Did the professor take attendance today?'.<br> She says, 'Oh good, and yes. The professor took attendance today.'",
 },
 {
@@ -61,7 +75,7 @@ var stimuli = [
 	foil: "<strong>Maya</strong>, he knows, only occasionally washes her dishes.",
 	likely: "Alicia",
 	unlikely: "Maya",
-	speakermanipulation: "Sophia and Matt are having lunch at Sophia's apartment.  Sophia has a chore chart she needs to fill out for the apartment, so she asks Matt if he can write down information while she prepares some food.  Matt reads out loud the first item on the chart, which concerns washing dishes.  He asks, 'Did your roommate wash her dishes today?'  Sophia replies, 'Yes, my roommate washed her dishes today.'",
+	speakermanipulation: "Sophia and Matt are having lunch at Sophia's apartment. Matt is helping Sophia fill out a chore chart for the apartment, while she prepares food.  Matt reads out loud, 'Did your roommate wash her dishes today?'  Sophia replies, 'Yes, my roommate washed her dishes today.'",
 	speakermanipulation2: "Sophia and Matt are having lunch at Sophia's apartment.<br> Matt asks Sophia, 'How are things going with your roommmate? Did she wash her dishes today?' <br> Sophia says, 'Oh good, and yes. My roommate washed her dishes today.'",
 },
 {
@@ -98,9 +112,9 @@ var stimuli = [
 },
 {
 	item: "maid_dusting",
-	prompt: "Henry is having a hard time remembering which daily cleaning service his friend Phil's maid works for.<br>He knows it is either Maid By Me or Sunflowers Maid Service.",
-	pragmatic2: "Henry visits Phil at his apartment. Henry asks, 'How are things around here?'<br>Phil says, 'Oh good. My maid dusted the windows today.'",
-	literal: "Henry visits Phil at his apartment. He inspects the windows. <br>The maid dusted the windows today.",
+	prompt: "Henry is having a hard time remembering which weekly cleaning service his friend Phil's maid works for.<br>He knows it is either Maid By Me or Sunflowers Maid Service.",
+	pragmatic2: "Henry visits Phil at his apartment. <br>Phil says, 'My maid dusted the windows this week.'",
+	literal: "Henry visits Phil at his apartment. He inspects the windows. <br>The maid dusted the windows this week.",
 	question: "What cleaning service does Phil's maid work for?",
 	frequency: {
 		high: "<strong>Maid By Me</strong>, he knows, always dusts the windows.",
@@ -109,8 +123,9 @@ var stimuli = [
 	foil: "<strong>Sunflowers Maid Service</strong>, he knows, only occasionally dusts the windows.",
 	likely: "Maid By Me",
 	unlikely: "Sunflowers Maid Service",
-	speakermanipulation: "Henry visits Phil at his apartment.  Phil asks Henry if he can help him fill out an evaluation of his maid who just cleaned his apartment while he prepares some food.  Henry reads out loud the first question on the form, which is, 'Did your maid dust the windows today?'  Phil replies, 'Yes. My maid dusted the windows today.'",
-	speakermanipulation2: "Henry visits Phil at his apartment.  Henry asks, 'How are things around here? Did your maid dust the windows today?'<br> Phil replies, 'Oh good, and yes. My maid dusted the windows today.'"
+	// speakermanipulation: "Henry visits Phil at his apartment.  Phil asks Henry if he can help him fill out an evaluation of his maid who just cleaned his apartment while he prepares some food.  Henry reads out loud the first question on the form, which is, 'Did your maid dust the windows today?'  Phil replies, 'Yes. My maid dusted the windows today.'",
+	speakermanipulation: "Henry visits Phil at his apartment.  Henry is helping Phil fill out a survey for the city's union of maid services, while Phil makes a snack. Henry reads out loud, 'Did your maid dust the windows this week?'<br> Phil replies, 'Yes. My maid dusted the windows this week.'"
+	// speakermanipulation: "X and Y meet up at Y's apartment to play basketball. <br>X is helping Y fill out a survey for the city union of c census, while X gets ready.  Y reads out loud, 'Did your tenant pay his rent on time this month?'  X replies, 'Yes, my tenant paid his rent on time this month.'",
 },
 {
 	item: "secretary_report",
@@ -150,9 +165,9 @@ var stimuli = [
 {
 	item: "doctor_writing",
 	prompt: "Rebecca is having a hard time remembering who her friend Jennifer's doctor is.<br>She knows it is either Dr. Smith or Dr. Jones.",
-	pragmatic2: "When Jennifer comes back from her doctor's appointment, Rebeccas asks her, 'How did your appointment go?'<br>Jennifer says, 'Oh good. My doctor wrote legibly today.'",
+	pragmatic: "When Jennifer comes back from getting a prescription from her doctor, Jennifer tells Rebecca, 'My doctor wrote legibly today.'",
 	// pragmatic2: "When Jennifer comes back from her doctor's appointment, Rebecca asks her: 'How was the appointment?' Jennifer says, 'It went well. My doctor wrote legibly today.'",
-	literal: "When Jennifer comes back from her doctor's appointment, she takes out her prescription and leaves it on the table.  Rebecca looks at the handwriting on the prescription.<br>Jennifer's doctor wrote legibly today.",
+	literal: "When Jennifer comes back from her doctor's appointment, she takes out her prescription and leaves it on the table.  Rebecca looks at the prescription.<br>Jennifer's doctor wrote legibly today.",
 	question: "Who is Jennifer's doctor?",
 	frequency: {
 		high: "<strong>Dr. Smith</strong>, she knows, always writes legibly.",
@@ -162,7 +177,7 @@ var stimuli = [
 	likely: "Dr. Smith",
 	unlikely: "Dr. Jones",
 	speakermanipulation: "When Jennifer comes back from her doctor's appointment, she asks Rebecca if she can help her fill out an on-line review of the doctor since her hand is in a bandage.  Rebecca reads the questions out loud to Jennifer.  She asks, 'Did the doctor write legibly?'  Rebecca replies, 'Yes, my doctor wrote legibly today.'",
-	speakermanipulation2: "When Jennifer comes back from her doctor's appointment, Rebecca asks her: 'How did your appointment go? Did the doctor write legibly?'<br>Rebecca replies, 'Oh good, and yes. My doctor wrote legibly today.'"
+	speakermanipulation2: "Rebecca is over at Jennifer's apartment. Rebecca is helping Jennifer fill out a survey for the city's union of medical services, while Jennifer makes a snack. Rebecca asks her: 'Did your doctor write legibly today?'<br>Rebecca replies, 'Yes. My doctor wrote legibly today.'"
 },
 {
 	item: "italian_bread",
