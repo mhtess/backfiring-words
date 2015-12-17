f38929f954c04287a05b4e150c0f6f025ce5510e
mhtess <mtessler@stanford.edu>
2015-12-17 15:32:58 -0800

    test batch uniqturker

--- experiments/2afc/js/forcedchoice-1.js
+++ experiments/2afc/js/forcedchoice-1.js
@@ -228,7 +228,7 @@ function init() {
 
   repeatWorker = false;
   (function(){
-      var ut_id = "mht-backfire-test-20151217";
+      var ut_id = "mht-backfire-test-20151217a";
       if (UTWorkerLimitReached(ut_id)) {
         $('.slide').empty();
         repeatWorker = true;
