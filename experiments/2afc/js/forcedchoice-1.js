0b6a4210262d9308f81daa85900132021a03b91f
mhtess <mtessler@stanford.edu>
2015-12-18 09:49:14 -0800

    add uniq id + warning

--- experiments/2afc/js/forcedchoice-1.js
+++ experiments/2afc/js/forcedchoice-1.js
@@ -228,7 +228,7 @@ function init() {
 
   repeatWorker = false;
   (function(){
-      var ut_id = "mht-backfire-test-20151217d";
+      var ut_id = "mht-backfire-20151218a  ";
       if (UTWorkerLimitReached(ut_id)) {
         $('.slide').empty();
         repeatWorker = true;
