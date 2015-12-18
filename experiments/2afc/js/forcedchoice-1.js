b9a40da04bdbc59d6dd73e6c021a5c9af9702766
mhtess <mtessler@stanford.edu>
2015-12-17 16:33:38 -0800

    test3

--- experiments/2afc/js/forcedchoice-1.js
+++ experiments/2afc/js/forcedchoice-1.js
@@ -228,7 +228,7 @@ function init() {
 
   repeatWorker = false;
   (function(){
-      var ut_id = "mht-backfire-test-20151217c";
+      var ut_id = "mht-backfire-test-20151217d";
       if (UTWorkerLimitReached(ut_id)) {
         $('.slide').empty();
         repeatWorker = true;
