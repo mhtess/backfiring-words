2e2f6267d43823a0a87ed815094fa18eea2dc14a
elliechestnut <eleanor.chestnut@gmail.com>
2016-04-02 10:23:30 -0700

    Update stims-3.js

--- experiments/priors/js/stims-3.js
+++ experiments/priors/js/stims-3.js
@@ -5,4 +5,52 @@ var stimuli = [
 	past_participle: "used their cell phone", // e.g. have __ before?
 	interval: "day"
 }
+{
+	verb: "wear shoes", // e.g. how often does he or she ___ ?
+	past: "wore shoes", // e.g. ___ yesterday?
+	past_participle: "worn shoes", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear a shirt", // e.g. how often does he or she ___ ?
+	past: "wore a shirt", // e.g. ___ yesterday?
+	past_participle: "worn a shirt", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear shoes", // e.g. how often does he or she ___ ?
+	past: "wore shoes", // e.g. ___ yesterday?
+	past_participle: "worn shoes", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear their glasses", // e.g. how often does he or she ___ ?
+	past: "wore their glasses", // e.g. ___ yesterday?
+	past_participle: "worn worn their glasses", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "eat breakfast", // e.g. how often does he or she ___ ?
+	past: "ate breakfast", // e.g. ___ yesterday?
+	past_participle: "eaten breakfast", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "drink coffee", // e.g. how often does he or she ___ ?
+	past: "drank coffee", // e.g. ___ yesterday?
+	past_participle: "drunk coffee", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "eat meat", // e.g. how often does he or she ___ ?
+	past: "ate meat", // e.g. ___ yesterday?
+	past_participle: "eaten meat", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear shoes", // e.g. how often does he or she ___ ?
+	past: "wore shoes", // e.g. ___ yesterday?
+	past_participle: "worn shoes", // e.g. have __ before?
+	interval: "day"
+}
 ]
