db4616302492eecb2ef145d9f263b0517d59d88e
elliechestnut <eleanor.chestnut@gmail.com>
2016-04-02 10:30:25 -0700

    Update stims-3.js

--- experiments/priors/js/stims-3.js
+++ experiments/priors/js/stims-3.js
@@ -48,6 +48,96 @@ var stimuli = [
 	interval: "day"
 }
 {
+	verb: "go outside", // e.g. how often does he or she ___ ?
+	past: "went outside", // e.g. ___ yesterday?
+	past_participle: "gone outside", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "drive their car", // e.g. how often does he or she ___ ?
+	past: "drove their car", // e.g. ___ yesterday?
+	past_participle: "driven their car", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wash their dishes", // e.g. how often does he or she ___ ?
+	past: "washed their dishes", // e.g. ___ yesterday?
+	past_participle: "washed their dishes", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "exercise", // e.g. how often does he or she ___ ?
+	past: "exercised", // e.g. ___ yesterday?
+	past_participle: "exercised", // e.g. have __ before?
+	interval: "month"
+}
+{
+	verb: "change their bedsheets", // e.g. how often does he or she ___ ?
+	past: "changed their bedsheets", // e.g. ___ yesterday?
+	past_participle: "changed their bedsheets", // e.g. have __ before?
+	interval: "week"
+}
+{
+	verb: "have a birthday party", // e.g. how often does he or she ___ ?
+	past: "had a birthday party", // e.g. ___ yesterday?
+	past_participle: "had a birthday party", // e.g. have __ before?
+	interval: "year"
+}
+{
+	verb: "pay their taxes", // e.g. how often does he or she ___ ?
+	past: "paid their taxes", // e.g. ___ yesterday?
+	past_participle: "paid their taxes, // e.g. have __ before?
+	interval: "year"
+}
+{
+	verb: "wear shoes", // e.g. how often does he or she ___ ?
+	past: "wore shoes", // e.g. ___ yesterday?
+	past_participle: "worn shoes", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wash their hair", // e.g. how often does he or she ___ ?
+	past: "washed their hair", // e.g. ___ yesterday?
+	past_participle: "washed their hair", // e.g. have __ before?
+	interval: "week"
+}
+{
+	verb: "take a vacation", // e.g. how often does he or she ___ ?
+	past: "took a vacation", // e.g. ___ yesterday?
+	past_participle: "taken a vacation", // e.g. have __ before?
+	interval: "year"
+}
+{
+	verb: "wear shoes", // e.g. how often does he or she ___ ?
+	past: "wore shoes", // e.g. ___ yesterday?
+	past_participle: "worn shoes", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear sandals", // e.g. how often does he or she ___ ?
+	past: "wore sandals", // e.g. ___ yesterday?
+	past_participle: "worn sandals", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear tie-dye", // e.g. how often does he or she ___ ?
+	past: "wore tie-dye", // e.g. ___ yesterday?
+	past_participle: "worn tie-dye", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "wear a hat", // e.g. how often does he or she ___ ?
+	past: "wore a hat", // e.g. ___ yesterday?
+	past_participle: "worn a hat", // e.g. have __ before?
+	interval: "day"
+}
+{
+	verb: "eat pasta", // e.g. how often does he or she ___ ?
+	past: "ate pasta", // e.g. ___ yesterday?
+	past_participle: "eaten pasta", // e.g. have __ before?
+	interval: "day"
+}
+{
 	verb: "wear shoes", // e.g. how often does he or she ___ ?
 	past: "wore shoes", // e.g. ___ yesterday?
 	past_participle: "worn shoes", // e.g. have __ before?
