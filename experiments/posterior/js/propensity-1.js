364bf5458c5c3524a667e1558c1984546f0c99f1
mhtess <mtessler@stanford.edu>
2016-04-11 17:26:30 -0700

    Most of a simple backfiring expt

---
+++ experiments/posterior/js/propensity-1.js
@@ -0,0 +1,447 @@
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function make_slides(f) {
+  var slides = {};
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+      $(".total-num").html(exp.numTrials);  
+     }
+  });
+
+  slides.instructions = slide({
+    name : "instructions",
+     start: function() {
+      // _.mapObject(exp.judgeButtons, function(val,key){
+      //   $("#"+key).html(val);
+      // });
+      var instructions = {
+        baseline: "On each trial, you will be asked how often a typical person does an action.",
+        observation: "On each trial, you will be given a random fact about a person doing an action and be asked to judge how often that person does the action.",
+        communication: "On each trial, you will read a brief excerpt from a conversation between two friends and be asked a question."
+      }
+      $(".instructions").html(instructions[exp.condition]);  
+      $(".total-num").html(exp.numTrials);  
+    },
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.truthJudge = slide({
+    name: "truthJudge",
+    present : exp.stimuli,
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+
+       $(".err").hide();
+       $('input[name="response"]').attr("checked", false);
+
+      this.judgeButtons = _.object(_.zip(["likely-key","unlikely-key","zero-key"],
+                                _.shuffle(["A","B","C"])));
+      this.startTime = Date.now();
+      this.stim = stim;
+
+      var observationSentence = stim.character.name + " " + stim.past + " today."
+
+      // var communicationSentence = 
+
+
+      // var baselineSentence = stim.character.name + " is a person."
+
+      var possible_sentences = {
+        observation: observationSentence,
+        communication: 'You overhear two friends talking. <br>One of them says to the other, "' + observationSentence +'"',
+        baseline: stim.character.name + " is a person."
+      }
+      var targetSentence = possible_sentences[exp.condition]
+
+      // exp.condition == "observation" ?
+      //                         observationSentence : 
+      //                         exp.condition == "communication" ?
+      //                         communicationSentence :
+      //                         baselineSentence
+
+      $(".prompt").html(targetSentence)
+      $(".question").html("Which do you think is most likely?")
+
+      var keyDictionary = {
+        "likely-key": "Relative to other people, " +stim.character.name + " " + stim.habitual  + " often.",
+        "unlikely-key": "Relative to other people, "+ stim.character.name + " <strong>does not</strong> " + stim.verb  + " often.",
+        "zero-key":stim.character.name + " <strong>never</strong> " + stim.habitual  + ".",
+      }
+
+      _.mapObject(this.judgeButtons, function(val,key){
+        $("#"+val+"key-reminder").html(keyDictionary[key]);
+      });
+      this.init_sliders();
+      exp.sliderPost = null; //erase current slider value
+    },
+    init_sliders : function() {
+      utils.make_slider("#single_slider", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+
+    button : function() {
+     var response = $('input[name="response"]:checked').val()
+    if ( (typeof response == 'undefined') ||  (exp.sliderPost == null)  ){
+        $(".err").show();
+      } else {
+        this.rt = Date.now() - this.startTime;
+        this.log_responses();
+        _stream.apply(this);
+      }
+    },
+
+    log_responses : function() {
+      var response = _.invert(this.judgeButtons)[$('input[name="response"]:checked').val()]
+      var person = response == "likely-key" ? this.stim.likely : this.stim.unlikely
+
+      exp.data_trials.push({
+        "trial_type" : "truthJudge",
+        "item": this.stim.item,
+        "condition": this.stim.condition,
+        "question": this.stim.question,
+        "prompt":this.stim.prompt,
+        "response" : response,
+        "confidence": exp.sliderPost,
+        "likely_person":this.stim.likely,
+        "unlikely_person": this.stim.unlikely,
+        "person": person,
+        "likely_info":this.stim.frequency.high,
+        "unlikely_info":this.stim.foil,
+        "rt":this.rt,
+        "personOrder":exp.personOrder
+      });
+    }
+  });
+
+  slides.likelihood_judgment = slide({
+    name: "likelihood_judgment",
+    present : exp.stimuli,
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+
+      $(".err").hide();
+
+      this.startTime = Date.now();
+      this.stim = stim;
+
+      var observationSentence = stim.character.name + " " + stim.past + " today."
+
+      var possible_sentences = {
+        observation: observationSentence,
+        communication: 'You overhear two friends talking. <br>One of them says to the other, "' + observationSentence +'"',
+        baseline: stim.character.name + " is a person."
+      }
+
+
+      var targetSentence = possible_sentences[exp.condition]
+
+      $(".prompt").html(targetSentence)
+      $(".question").html("How likely is it that "+stim.character.name +" will " + stim.verb + " in the next "+stim.predict_test_freq+"?")
+
+      this.init_sliders();
+      exp.sliderPost = null; //erase current slider value
+    },
+    init_sliders : function() {
+      utils.make_slider("#single_slider_2", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+
+    button : function() {
+     var response = $('input[name="response"]:checked').val()
+    if ( (exp.sliderPost == null)  ){
+        $(".err").show();
+      } else {
+        this.rt = Date.now() - this.startTime;
+        this.log_responses();
+        _stream.apply(this);
+      }
+    },
+
+    log_responses : function() {
+
+      exp.data_trials.push({
+        "trial_type" : "likelihood_judge",
+        "item": this.stim.past,
+        "condition": exp.condition,
+        "response": exp.sliderPost,
+        "characterGender": this.stim.character.gender,
+        "rt":this.rt
+      });
+    }
+  });
+
+
+  slides.frequency_judgment = slide({
+    name: "frequency_judgment",
+    
+    present: exp.stimuli,
+
+    present_handle : function(stim) {
+
+
+      $(".err").hide();
+      // $(".err0").hide();
+      $("#time_frequency").val('')
+      $("#time_comparison").val('')
+
+      this.startTime = Date.now();
+      this.stim = stim;
+      this.trialNum = exp.stimscopy.indexOf(stim);
+
+      var interval_dictionary = {
+        day: "today",
+        week: "this week",
+        month: "this month",
+        year: "this year"
+      }
+
+      var posessive_gender = {
+        male: "his",
+        female: "her"
+      }
+
+      this.observationSentence =  stim.possessive ? 
+          stim.character.name + " " + stim.past.replace("their",posessive_gender[stim.character.gender]) + 
+              " " + interval_dictionary[stim.interval] + "." : 
+          stim.character.name + " " + stim.past + " " + interval_dictionary[stim.interval] + "."
+
+      var possible_sentences = {
+        observation: this.observationSentence,
+        communication: 'You overhear two friends talking. <br>One of them says to the other, "' + this.observationSentence +'"',
+        baseline: ""
+      }
+      this.targetSentence = possible_sentences[exp.condition]
+
+      $(".prompt").html(this.targetSentence)
+      this.charName = exp.condition == "baseline" ? "a typical person" : stim.character.name
+
+      var question = stim.possessive ? 
+        "How often does "+this.charName + " " +
+              stim.verb.replace("their", posessive_gender[stim.character.gender]) + "?" :
+        "How often does "+this.charName + " " + stim.verb + "?"
+
+      $(".question").html(question)
+
+    },
+
+    button : function() {
+      var responses = {
+        ntimes: $("#time_frequency").val(),
+        time_window: $("#time_comparison").val()
+      };
+      $(".err").hide();
+      // $(".err0").hide();
+
+      if (responses.time_window==null || !(parseFloat(responses.ntimes) > -1))  {
+        $(".err").show();
+      // } else if (parseFloat(responses.ntimes)%1 != 0) {
+      //   $(".err0").show();
+      } else {
+        this.rt = Date.now() - this.startTime;
+        this.log_responses();
+        _stream.apply(this);
+      }
+    },
+
+    log_responses : function() {
+      var timeTransform = {
+        "day": 1,
+        "week":1/7,
+        "month":1/30,
+        "year":1/365,
+        "5 years":1/(365*5),
+      }
+      var responses = {
+        ntimes: $("#time_frequency").val(),
+        time_window: $("#time_comparison").val()
+      };
+      var day_rate = timeTransform[responses.time_window]*parseFloat(responses.ntimes);
+      exp.data_trials.push({
+        "trial_type" : "propensity_judgment",
+        "condition": exp.condition,
+        "trial_num": this.trialNum+1,
+        "past": this.stim.past,
+        "item": this.stim.verb,
+        "observationSentence": this.observationSentence,
+        "targetSentence": this.targetSentence,
+        "ntimes" : parseFloat(responses.ntimes),
+        "interval": responses.time_window,
+        "rt":this.rt,
+        "dayrate":day_rate,
+        "log_dayrate": Math.log(day_rate)
+      });
+    }
+  });
+
+
+  slides.catch_trial = slide({
+     name : "catch_trial",
+     start: function() {
+      this.startTime = Date.now();
+            $(".err").hide();
+     },
+    button : function() {
+      var response1 = $("#catch").val()
+      if (response1 == -1) {
+        $(".err").show();
+      } else {
+        this.rt = Date.now() - this.startTime;
+        this.log_responses();
+        exp.go();
+      }
+    },
+    log_responses : function() {
+      var correctAnswer = {
+        "observation": 1,
+        "communication":2,
+        "baseline":0
+      }
+      var response1 = $("#catch").val()
+      var el = document.getElementById('catch');
+      var text = el.options[el.selectedIndex].innerHTML;
+      exp.catch_trials.push({
+        "trial_type" : "catch",
+        "condition" : exp.condition, 
+        "response" : response1,
+        "selection":text,
+        "pass": (response1 == correctAnswer[exp.condition])? 1 : 0,
+        "rt":this.rt
+      });
+    }
+  });
+
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        language : $("#language").val(),
+        enjoyment : $("#enjoyment").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        problems: $("#problems").val(),
+        fairprice: $("#fairprice").val(),
+        comments : $("#comments").val()
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          "condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+
+  repeatWorker = false;
+  (function(){
+      var ut_id = "mht-backfire-20160123";
+      if (UTWorkerLimitReached(ut_id)) {
+        $('.slide').empty();
+        repeatWorker = true;
+        alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
+      }
+  })();
+
+  exp.trials = [];
+  exp.catch_trials = [];
+
+
+
+
+
+  exp.condition = _.sample(["baseline","observation","communication"])
+  exp.condition = "communication"
+
+
+  var allGenders = _.shuffle(_.flatten([maleCharacters, femaleCharacters]))
+
+
+  var stims_w_names =  _.shuffle(_.flatten(_.map(stimuli, function(s){
+    var newObj = jQuery.extend(true, {}, s);
+    return _.extend(s, {character: allGenders.pop()})  
+  }), true))
+
+  exp.stimuli = _.shuffle(stims_w_names);
+  exp.stimscopy = exp.stimuli.slice(0)
+  // debugger;
+  exp.personOrder = _.sample(["likely-unlikely","unlikely-likely"])
+  exp.buttonCodes = {80:"P", 81:"Q"};
+  // exp.judgeButtons = _.object(_.zip(["likely-key","unlikely-key"],
+  //                           _.shuffle(["P","Q"])));
+
+
+  // debugger;
+  exp.numTrials = exp.stimuli.length;
+
+
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+  };
+
+  //blocks of the experiment:
+   exp.structure=["i0", "instructions","catch_trial","frequency_judgment",'subj_info', 'thanks'];
+ 
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
--- experiments/priors/js/stims-3.js
+++ experiments/priors/js/stims-3.js
@@ -3,91 +3,106 @@ var stimuli = [
 	verb: "use a cell phone", // e.g. how often does he or she ___ ?
 	past: "used a cell phone", // e.g. ___ yesterday?
 	past_participle: "used a cell phone", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "wear shoes", // e.g. how often does he or she ___ ?
 	past: "wore shoes", // e.g. ___ yesterday?
 	past_participle: "worn shoes", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "wear a shirt", // e.g. how often does he or she ___ ?
 	past: "wore a shirt", // e.g. ___ yesterday?
 	past_participle: "worn a shirt", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "wear glasses", // e.g. how often does he or she ___ ?
 	past: "wore glasses", // e.g. ___ yesterday?
 	past_participle: "worn glasses", // esho.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "buy their own groceries", // e.g. how often does he or she ___ ?
 	past: "bought their own groceries", // e.g. ___ yesterday?
 	past_participle: "bought their own groceries", // e.g. have __ before?
-	interval: "week"
+	interval: "week",
+	possessive: true
 },
 {
 	verb: "eat breakfast", // e.g. how often does he or she ___ ?
 	past: "ate breakfast", // e.g. ___ yesterday?
 	past_participle: "eaten breakfast", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "drink coffee", // e.g. how often does he or she ___ ?
 	past: "drank coffee", // e.g. ___ yesterday?
 	past_participle: "drunk coffee", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "eat meat", // e.g. how often does he or she ___ ?
 	past: "ate meat", // e.g. ___ yesterday?
 	past_participle: "eaten meat", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "go outside", // e.g. how often does he or she ___ ?
 	past: "went outside", // e.g. ___ yesterday?
 	past_participle: "gone outside", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "drive a car", // e.g. how often does he or she ___ ?
 	past: "drove a car", // e.g. ___ yesterday?
 	past_participle: "driven a car", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "wash their dishes", // e.g. how often does he or she ___ ?
 	past: "washed their dishes", // e.g. ___ yesterday?
 	past_participle: "washed their dishes", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: true
 },
 {
 	verb: "exercise", // e.g. how often does he or she ___ ?
 	past: "exercised", // e.g. ___ yesterday?
 	past_participle: "exercised", // e.g. have __ before?
-	interval: "month"
+	interval: "month",
+	possessive: false
 },
 {
 	verb: "change their bedsheets", // e.g. how often does he or she ___ ?
 	past: "changed their bedsheets", // e.g. ___ yesterday?
 	past_participle: "changed their bedsheets", // e.g. have __ before?
-	interval: "week"
+	interval: "week",
+	possessive: true
 },
 {
 	verb: "have a birthday party", // e.g. how often does he or she ___ ?
 	past: "had a birthday party", // e.g. ___ yesterday?
 	past_participle: "had a birthday party", // e.g. have __ before?
-	interval: "year"
+	interval: "year",
+	possessive: false
 },
 {
 	verb: "pay their taxes", // e.g. how often does he or she ___ ?
 	past: "paid their taxes", // e.g. ___ yesterday?
 	past_participle: "paid their taxes", // e.g. have __ before?
-	interval: "year"
+	interval: "year",
+	possessive: true
 },
 //{
 //	verb: "wear shoes", // e.g. how often does he or she ___ ?
@@ -99,60 +114,488 @@ var stimuli = [
 	verb: "wash their hair", // e.g. how often does he or she ___ ?
 	past: "washed their hair", // e.g. ___ yesterday?
 	past_participle: "washed their hair", // e.g. have __ before?
-	interval: "week"
+	interval: "week",
+	possessive: true
 },
 {
 	verb: "take a vacation", // e.g. how often does he or she ___ ?
 	past: "took a vacation", // e.g. ___ yesterday?
 	past_participle: "taken a vacation", // e.g. have __ before?
-	interval: "year"
+	interval: "year",
+	possessive: false
 },
 {
 	verb: "wear tie-dye", // e.g. how often does he or she ___ ?
 	past: "wore tie-dye", // e.g. ___ yesterday?
 	past_participle: "worn tie-dye", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "wear a hat", // e.g. how often does he or she ___ ?
 	past: "wore a hat", // e.g. ___ yesterday?
 	past_participle: "worn a hat", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "eat pasta", // e.g. how often does he or she ___ ?
 	past: "ate pasta", // e.g. ___ yesterday?
 	past_participle: "eaten pasta", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "drink lemonade", // e.g. how often does he or she ___ ?
 	past: "drank lemonade", // e.g. ___ yesterday?
 	past_participle: "drunk lemonade", // e.g. have __ before?
-	interval: "day"
+	interval: "day",
+	possessive: false
 },
 {
 	verb: "go on a hike", // e.g. how often does he or she ___ ?
 	past: "went on a hike", // e.g. ___ yesterday?
 	past_participle: "gone on a hike", // e.g. have __ before?
-	interval: "month"
+	interval: "month",
+	possessive: false
 },
 {
 	verb: "go to Renaissance fairs", // e.g. how often does he or she ___ ?
 	past: "went to a Renaissance fair", // e.g. ___ yesterday?
 	past_participle: "gone to a Renaissance fair", // e.g. have __ before?
-	interval: "year"
+	interval: "year",
+	possessive: false
 },
 {
 	verb: "go skiing", // e.g. how often does he or she ___ ?
 	past: "went skiing", // e.g. ___ yesterday?
 	past_participle: "gone skiing", // e.g. have __ before?
-	interval: "year"
+	interval: "year",
+	possessive: false
 },
 {
 	verb: "take dance lessons", // e.g. how often does he or she ___ ?
 	past: "took dance lessons", // e.g. ___ yesterday?
 	past_participle: "taken dance lessons", // e.g. have __ before?
-	interval: "week"
+	interval: "week",
+	possessive: false
 }
 ]
+
+var characters = [
+{
+	name: "Daniel",
+	gender: "male"
+},
+{
+	name: "David",
+	gender: "male"
+},
+{
+	name: "Alexander",
+	gender: "male"
+},
+{
+	name: "Gabriel",
+	gender: "male"
+},
+{
+	name: "Anthony",
+	gender: "male"
+},
+{
+	name: "William",
+	gender: "male"
+},
+{
+	name: "John",
+	gender: "male"
+},
+{
+	name: "Tom",
+	gender: "male"
+},
+{
+	name: "Lucas",
+	gender: "male"
+},
+{
+	name: "Michael",
+	gender: "male"
+},
+{
+	name: "Eric",
+	gender: "male"
+},
+{
+	name: "Cameron",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Lee",
+	gender: "male"
+},
+{
+	name: "Greg",
+	gender: "male"
+},
+{
+	name: "Angela",
+	gender: "female"
+},
+{
+	name: "Jennifer",
+	gender: "female"	
+},
+{
+	name: "Monica",
+	gender: "female"
+},
+{
+	name: "Melinda",
+	gender: "female"
+},
+{
+	name: "Veronica",
+	gender: "female"	
+},
+{
+	name: "Alicia",
+	gender: "female"
+},
+{
+	name: "Maya",
+	gender: "female"
+},
+{
+	name: "Sophia",
+	gender: "female"
+},
+{
+	name: "Kim",
+	gender: "female"
+},
+{
+	name: "Laura",
+	gender: "female"
+},
+{
+	name: "Julia",
+	gender: "female"
+},
+{
+	name: "Michelle",
+	gender: "female"
+},
+{
+	name: "Stephanie",
+	gender: "female"
+},
+{
+	name: "Claire",
+	gender: "female"
+},
+{
+	name: "Kathleen",
+	gender: "female"
+}]	
+
+
+var maleCharacters = [
+{
+	name: "Daniel",
+	gender: "male"
+},
+{
+	name: "David",
+	gender: "male"
+},
+{
+	name: "Alexander",
+	gender: "male"
+},
+{
+	name: "Gabriel",
+	gender: "male"
+},
+{
+	name: "Anthony",
+	gender: "male"
+},
+{
+	name: "William",
+	gender: "male"
+},
+{
+	name: "John",
+	gender: "male"
+},
+{
+	name: "Tom",
+	gender: "male"
+},
+{
+	name: "Lucas",
+	gender: "male"
+},
+{
+	name: "Michael",
+	gender: "male"
+},
+{
+	name: "Eric",
+	gender: "male"
+},
+{
+	name: "Cameron",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Lee",
+	gender: "male"
+},
+{
+	name: "Greg",
+	gender: "male"
+},
+{
+	name: "Kyle",
+	gender: "male"
+},
+{
+	name: "Ben",
+	gender: "male"
+},
+{
+	name: "Josh",
+	gender: "male"
+},
+{
+	name: "Robert",
+	gender: "male"
+},
+{
+	name: "Ronald",
+	gender: "male"
+},
+{
+	name: "Jacob",
+	gender: "male"
+},
+{
+	name: "Lewis",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Derrick",
+	gender: "male"
+},
+{
+	name: "Christopher",
+	gender: "male"
+},
+{
+	name: "Brian",
+	gender: "male"
+},
+{
+	name: "Vince",
+	gender: "male"
+},
+{
+	name: "Ted",
+	gender: "male"
+},
+{
+	name: "Scott",
+	gender: "male"
+},
+{
+	name: "Seth",
+	gender: "male"
+},
+{
+	name: "Martin",
+	gender: "male"
+},
+{
+	name: "Doug",
+	gender: "male"
+},
+{
+	name: "George",
+	gender: "male"
+},
+{
+	name: "Tim",
+	gender: "male"
+},
+{
+	name: "Jack",
+	gender: "male"
+},
+{
+	name: "Fred",
+	gender: "male"
+}]
+
+
+
+var femaleCharacters = [
+{
+	name: "Angela",
+	gender: "female"
+},
+{
+	name: "Jennifer",
+	gender: "female"	
+},
+{
+	name: "Monica",
+	gender: "female"
+},
+{
+	name: "Melinda",
+	gender: "female"
+},
+{
+	name: "Veronica",
+	gender: "female"	
+},
+{
+	name: "Alicia",
+	gender: "female"
+},
+{
+	name: "Maya",
+	gender: "female"
+},
+{
+	name: "Sophia",
+	gender: "female"
+},
+{
+	name: "Kim",
+	gender: "female"
+},
+{
+	name: "Laura",
+	gender: "female"
+},
+{
+	name: "Julia",
+	gender: "female"
+},
+{
+	name: "Michelle",
+	gender: "female"
+},
+{
+	name: "Stephanie",
+	gender: "female"
+},
+{
+	name: "Claire",
+	gender: "female"
+},
+{
+	name: "Kathleen",
+	gender: "female"
+},
+{
+	name: "Rebecca",
+	gender: "female"
+},
+{
+	name: "Hillary",
+	gender: "female"
+},
+{
+	name: "Jessica",
+	gender: "female"
+},
+{
+	name: "Natalie",
+	gender: "female"
+},
+{
+	name: "Kristina",
+	gender: "female"
+},
+{
+	name: "Esther",
+	gender: "female"
+},
+{
+	name: "Elizabeth",
+	gender: "female"
+},
+{
+	name: "Crystal",
+	gender: "female"
+},
+{
+	name: "Theresa",
+	gender: "female"
+},
+{
+	name: "Janet",
+	gender: "female"
+},
+{
+	name: "Linda",
+	gender: "female"
+},
+{
+	name: "Elisa",
+	gender: "female"
+},
+{
+	name: "Ashley",
+	gender: "female"
+},
+{
+	name: "Shannon",
+	gender: "female"
+},
+{
+	name: "Tina",
+	gender: "female"
+},
+{
+	name: "Kelly",
+	gender: "female"
+},
+{
+	name: "Bridgette",
+	gender: "female"
+},
+{
+	name: "Amber",
+	gender: "female"
+},
+{
+	name: "Jackie",
+	gender: "female"
+},
+{
+	name: "Brooke",
+	gender: "female"
+},
+{
+	name: "Nicole",
+	gender: "female"
+}]	
+
